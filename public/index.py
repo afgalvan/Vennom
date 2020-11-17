b2f7cc376486e1da58e7e06825b873c615b6c257
Andres <andresgalfajar@gmail.com>
2020-11-16 21:39:33 -0500

    Change file name from 'main' to 'index'

---
+++ public/index.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from public.buttons import *
+
+
+def main():
+    # setting up the tkinter window
+    root = Tk()
+    icon = PhotoImage(file="public/assets/vennom_icon.png")
+    root.iconphoto(False, icon)
+    root.geometry("250x400+300+300")
+    root.resizable(0, 0)
+    root.title("VENNOM")
+    components(root)
+    root.mainloop()
